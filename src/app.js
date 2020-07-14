7214435a7dba436057bd9ea7be649d45a65ae218
CarvalhoBr <brandonsouza27@gmail.com>
2020-07-14 17:10:40 -0300

    Desafio concluído

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -12,22 +12,85 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const {title, url, techs} = request.body
+  const id = uuid()
+  const likes = 0
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories.push(repository)
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const {
+    title,
+    url,
+    techs
+  } = request.body
+
+  const { id } = request.params
+  const rId = repositories.findIndex(repository => repository.id === id)
+
+  if(rId < 0){
+    return response.status(400).end()
+    
+  }
+  
+  const likes = repositories[rId].likes
+
+  repositories[rId] = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  return response.json(repositories[rId])
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params
+  const rId = repositories.findIndex(repository => repository.id == id)
+
+  if(rId < 0){
+    return response.status(400).json({"[ERROR]": "Invalid ID"})
+  }
+
+  repositories.splice(rId, 1)
+
+  return response.status(204).end()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+  const { id } = request.params
+  const rId = repositories.findIndex(repository => repository.id === id)
+  console.log(id, rId)
+
+  if(rId < 0){
+    return response.status(400).json({"[ERROR]": "Invalid ID"})
+  }
+
+  repositories[rId].likes ++
+
+  response.json({"likes": repositories[rId].likes})
 });
 
 module.exports = app;
